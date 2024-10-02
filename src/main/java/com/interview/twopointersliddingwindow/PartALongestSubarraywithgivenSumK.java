dd8f529ac52fe69e5c291a8e0cb8ff82a0a87c49
mukeshgupta <mukesh_gupta85@yahoo.com>
2024-10-02 09:51:26 +0530

    code refactoring

---
+++ src/main/java/com/interview/twopointersliddingwindow/PartALongestSubarraywithgivenSumK.java
@@ -0,0 +1,153 @@
+package com.interview.twopointersliddingwindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+https://www.youtube.com/watch?v=9kdHxplyl5I&list=PLgUwDviBIf0q7vrFA_HEWcqRqMpCXzYAL
+ */
+public class PartALongestSubarraywithgivenSumK {
+    /*
+    Complexity Analysis
+Time Complexity: O(N2) approx., where N = size of the array.
+Reason: We are using two nested loops, each running approximately N times.
+
+Space Complexity: O(1) as we are not using any extra space.
+     */
+    public static int getLongestSubarrayBruiforce(int []a, long k) {
+        int n = a.length; // size of the array.
+
+        int len = 0;
+        for (int i = 0; i < n; i++) { // starting index
+            long s = 0; // Sum variable
+            for (int j = i; j < n; j++) { // ending index
+                // add the current element to
+                // the subarray a[i...j-1]:
+                s += a[j];
+
+                if (s == k)
+                    len = Math.max(len, j - i + 1);
+            }
+        }
+        return len;
+    }
+
+    /*
+    Time Complexity: O(N) or O(N*logN) depending on which map data structure we are using, where N = size of the array.
+Reason: For example, if we are using an unordered_map data structure in C++ the time complexity will be O(N)(though in the worst case, unordered_map takes O(N) to find an element and the time complexity becomes O(N2)) but if we are using a map data structure, the time complexity will be O(N*logN). The least complexity will be O(N) as we are using a loop to traverse the array.
+
+Space Complexity: O(N) as we are using a map data structure.
+     */
+
+    public static int getLongestSubarrayBetter(int []a, long k) {
+        int n = a.length; // size of the array.
+
+        Map<Long, Integer> preSumMap = new HashMap<>();
+        long sum = 0;
+        int maxLen = 0;
+        for (int i = 0; i < n; i++) {
+            //calculate the prefix sum till index i:
+            sum += a[i];
+
+            // if the sum = k, update the maxLen:
+            if (sum == k) {
+                maxLen = Math.max(maxLen, i + 1);
+            }
+
+            // calculate the sum of remaining part i.e. x-k:
+            long rem = sum - k;
+
+            //Calculate the length and update maxLen:
+            if (preSumMap.containsKey(rem)) {
+                int len = i - preSumMap.get(rem);
+                maxLen = Math.max(maxLen, len);
+            }
+
+            //Finally, update the map checking the conditions:
+            if (!preSumMap.containsKey(sum)) {
+                preSumMap.put(sum, i);
+            }
+        }
+
+        return maxLen;
+    }
+
+    /*
+    Time Complexity: O(2*N), where N = size of the given array.
+Reason: The outer while loop i.e. the right pointer can move up to index n-1(the last index). Now, the inner while loop i.e. the left pointer can move up to the right pointer at most. So, every time the inner loop does not run for n times rather it can run for n times in total. So, the time complexity will be O(2*N) instead of O(N2).
+
+Space Complexity: O(1) as we are not using any extra space.
+
+Space Complexity: O(1) as we are not using any extra space.
+     */
+    public static int getLongestSubarrayOptimal(int []a, long k) {
+        int n = a.length; // size of the array.
+
+        int left = 0, right = 0; // 2 pointers
+        long sum = a[0];
+        int maxLen = 0;
+        while (right < n) {
+            // if sum > k, reduce the subarray from left
+            // until sum becomes less or equal to k:
+            while (left <= right && sum > k) {
+                sum -= a[left];
+                left++;
+            }
+
+            // if sum = k, update the maxLen i.e. answer:
+            if (sum == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+
+            // Move forward thw right pointer:
+            right++;
+            if (right < n) sum += a[right];
+        }
+
+        return maxLen;
+    }
+
+        /*
+    Time Complexity: O(N), where N = size of the given array.
+Reason: The outer while loop i.e. the right pointer can move up to index n-1(the last index). Now, the inner while loop i.e. the left pointer can move up to the right pointer at most. So, every time the inner loop does not run for n times rather it can run for n times in total. So, the time complexity will be O(2*N) instead of O(N2).
+
+Space Complexity: O(1) as we are not using any extra space.
+
+Space Complexity: O(1) as we are not using any extra space.
+     */
+
+    public static int getLongestSubarrayOptimalV2(int []a, long k) {
+        int n = a.length; // size of the array.
+
+        int left = 0, right = 0; // 2 pointers
+        long sum = a[0];
+        int maxLen = 0;
+        while (right < n) {
+            // if sum > k, reduce the subarray from left
+            // until sum becomes less or equal to k:
+            if (left <= right && sum > k) {
+                sum -= a[left];
+                left++;
+            }
+
+            // if sum = k, update the maxLen i.e. answer:
+            if (sum == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+
+            // Move forward thw right pointer:
+            right++;
+            if (right < n) sum += a[right];
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 3, 5, 1, 9};
+        long k = 10;
+        int len = getLongestSubarrayBruiforce(a, k);
+        System.out.println("The length of the longest subarray is: " + len);
+    }
+
+}
